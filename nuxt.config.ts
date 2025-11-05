24c52633ac15ee7d2ca9c8232ce4cde84235958f
aliemuhammad86 <aliemuhammad847@gmail.com>
2025-11-05 13:22:43 +0700

    `Update nuxt.config.ts: import getProjectRoutes, set ssr to false`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,9 @@
 import { defineNuxtConfig } from "nuxt/config";
-const getProjectRoutes = require("./scripts/get-project-routes");
+import getProjectRoutes from "./scripts/get-project-routes";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/ui', '@pinia/nuxt'],
