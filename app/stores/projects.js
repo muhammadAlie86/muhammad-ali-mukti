9b2bc2ea1db5e8a4fbae70760fc0c0f998cfd987
aliemuhammad86 <aliemuhammad847@gmail.com>
2025-11-14 15:57:21 +0700

    `Added multiple project entries to projects store`

--- app/stores/projects.js
+++ app/stores/projects.js
@@ -87,6 +87,187 @@ export const useProjectsStore = defineStore('projects', {
           'Hilt',
           'Testing (Expresso, MockK)'
       ]
+      
+
+      },
+       {
+        id: 6,
+        title: 'Mini ATM EDC Pinpad',
+        description: 'A personal portfolio website.',
+        image: 'ic_aceh.png',
+        technologies: [
+          'Kotlin',
+          'Modern Android Development (MAD)',
+          'Firebase',
+          'Clean Architecture',
+          'Coroutines & Flow',
+          'Jetpack Compose',
+          'Hilt',
+          'Testing (Expresso, MockK)'
+      ]
+      
+
+      },
+       {
+        id: 7,
+        title: 'Mini Atm All In One',
+        description: 'A personal portfolio website.',
+        image: 'ic_aio.png',
+        technologies: [
+          'Kotlin',
+          'Modern Android Development (MAD)',
+          'Firebase',
+          'Clean Architecture',
+          'Coroutines & Flow',
+          'Jetpack Compose',
+          'Hilt',
+          'Testing (Expresso, MockK)'
+      ]
+      
+
+      },
+       {
+        id: 8,
+        title: 'Bayar Bayar Lite',
+        description: 'A personal portfolio website.',
+        image: 'ic_fuelcard.png',
+        technologies: [
+          'Kotlin',
+          'Modern Android Development (MAD)',
+          'Firebase',
+          'Clean Architecture',
+          'Coroutines & Flow',
+          'Jetpack Compose',
+          'Hilt',
+          'Testing (Expresso, MockK)'
+      ]
+      
+
+      },
+       {
+        id: 9,
+        title: 'Bayar Bayar Plus',
+        description: 'A personal portfolio website.',
+        image: 'ic_bbp.png',
+        technologies: [
+          'Kotlin',
+          'Modern Android Development (MAD)',
+          'Firebase',
+          'Clean Architecture',
+          'Coroutines & Flow',
+          'Jetpack Compose',
+          'Hilt',
+          'Testing (Expresso, MockK)'
+      ]
+      
+
+      },
+       {
+        id: 10,
+        title: 'Mini ATM BPD Bali',
+        description: 'A personal portfolio website.',
+        image: 'ic_fuelcard.png',
+        technologies: [
+          'Kotlin',
+          'Modern Android Development (MAD)',
+          'Firebase',
+          'Clean Architecture',
+          'Coroutines & Flow',
+          'Jetpack Compose',
+          'Hilt',
+          'Testing (Expresso, MockK)'
+      ]
+      
+
+      },
+       {
+        id: 11,
+        title: 'Eform Officer',
+        description: 'A personal portfolio website.',
+        image: 'ic_eform.png',
+        technologies: [
+          'Kotlin',
+          'Modern Android Development (MAD)',
+          'Firebase',
+          'Clean Architecture',
+          'Coroutines & Flow',
+          'Jetpack Compose',
+          'Hilt',
+          'Testing (Expresso, MockK)'
+      ]
+      
+
+      },
+       {
+        id: 12,
+        title: 'Mini ATM HiBank',
+        description: 'A personal portfolio website.',
+        image: 'ic_hibank.png',
+        technologies: [
+          'Kotlin',
+          'Modern Android Development (MAD)',
+          'Firebase',
+          'Clean Architecture',
+          'Coroutines & Flow',
+          'Jetpack Compose',
+          'Hilt',
+          'Testing (Expresso, MockK)'
+      ]
+      
+
+      },
+       {
+        id: 13,
+        title: 'Mini ATM KB Star',
+        description: 'A personal portfolio website.',
+        image: 'ic_mab.png',
+        technologies: [
+          'Kotlin',
+          'Modern Android Development (MAD)',
+          'Firebase',
+          'Clean Architecture',
+          'Coroutines & Flow',
+          'Jetpack Compose',
+          'Hilt',
+          'Testing (Expresso, MockK)'
+      ]
+      
+
+      },
+       {
+        id: 14,
+        title: 'Mini ATM Mitra Bisa',
+        description: 'A personal portfolio website.',
+        image: 'ic_mitra_bisa.png',
+        technologies: [
+          'Kotlin',
+          'Modern Android Development (MAD)',
+          'Firebase',
+          'Clean Architecture',
+          'Coroutines & Flow',
+          'Jetpack Compose',
+          'Hilt',
+          'Testing (Expresso, MockK)'
+      ]
+      
+
+      },
+       {
+        id: 16,
+        title: 'Mini ATM Panin Bank',
+        description: 'A personal portfolio website.',
+        image: 'ic_panin.png',
+        technologies: [
+          'Kotlin',
+          'Modern Android Development (MAD)',
+          'Firebase',
+          'Clean Architecture',
+          'Coroutines & Flow',
+          'Jetpack Compose',
+          'Hilt',
+          'Testing (Expresso, MockK)'
+      ]
+      
 
       },
     ],
